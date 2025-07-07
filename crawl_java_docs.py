2d5f0a0dc399dc944db90256ec0e868caa18ff19
Adarsh <samaniadarsh@gmail.com>
2025-07-07 20:00:42 +0530

    All Dataset added

--- crawl_java_docs.py
+++ crawl_java_docs.py
@@ -16,11 +16,11 @@ async def crawl_java_docs(url):
         if no_results:
             print("⚠️ No data found.")
         else:
-            save_venues_to_csv(table_data, "Stack_methods.csv")
-            print(f"✅ Saved {len(table_data)} entries to 'Stack_methods.csv'")
+            save_venues_to_csv(table_data, "dataset/StringBuffer_methods.csv")
+            print(f"✅ Saved {len(table_data)} entries to 'StringBuffer_methods.csv'")
 
 async def main():
-    url = "https://docs.oracle.com/javase/8/docs/api/java/util/Stack.html"
+    url = "https://docs.oracle.com/javase/8/docs/api/java/lang/StringBuffer.html"
     await crawl_java_docs(url)
 
 if __name__ == "__main__":
